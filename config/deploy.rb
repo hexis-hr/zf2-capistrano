769eee3a8b01393ec19504f6990ff41cd63eadec
romalapin <roman.lapin@gmail.com>
2012-04-15 14:57:41 +0400

    Add custom tasks to capistrano deploy config; update .htaccess to look for maintanence.html

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,3 +12,35 @@ server "107.22.224.149", :app, :web, :db, :primary => true
 set :ssh_options, {:forward_agent => true, :port => 22}
 set :user, "deployer"
 set :use_sudo, false
+
+namespace :myproject do
+
+    task :symlink, :roles => :app do
+        run "mkdir -p #{shared_path}/vendor/ZendFramework/library"
+        run "chmod -R 755 #{shared_path}/vendor/ZendFramework/library"
+        run "ln -nfs /usr/local/ZendFramework-2.0.0beta3/library/Zend #{shared_path}/vendor/ZendFramework/library/Zend"
+        run "rm -rf #{release_path}/vendor"
+        run "ln -nfs #{shared_path}/vendor #{release_path}/vendor"
+    end
+
+    task :uploads, :roles => :app do
+        run "mkdir -p #{shared_path}/public/uploads"
+        run "chmod -R 775 #{shared_path}/public/uploads"
+        run "ln -nfs #{shared_path}/public/uploads #{release_path}/public/uploads"
+    end
+
+    task :disable do
+        run "mkdir -p #{shared_path}/public"
+        run "echo 'Site is on maintenance right now. Sorry.' > #{shared_path}/public/maintenance.html"
+        run "cp #{shared_path}/public/maintenance.html #{latest_release}/public/maintenance.html"
+    end
+
+    task :enable do
+        run "rm -f #{latest_release}/public/maintenance.html"
+    end
+
+end
+
+after "deploy:update_code", "myproject:disable"
+after "deploy:symlink", "myproject:enable"
+after "deploy:symlink", "myproject:symlink", "myproject:uploads"
